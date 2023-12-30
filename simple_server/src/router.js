9c3182f0494544e1ae23faf11bd9dd3816265c94
ashervogel17 <ashervogel17@gmail.com>
2023-12-29 16:06:23 -0800

    added home and /bucket_info routes to api

--- simple_server/src/router.js
+++ simple_server/src/router.js
@@ -6,6 +6,14 @@ dotenv.config();
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    res.send('Welcome to the Easy Store API!')
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 router.get('/upload/:objectKey', async (req, res) => {
   try {
     const objectKey = req.params.objectKey;
@@ -21,6 +29,17 @@ router.get('/download/:objectKey', async (req, res) => {
     const objectKey = req.params.objectKey;
     const url = await s3.generateDownloadURL(process.env.S3_BUCKET, objectKey, 180);
     res.send({ url })
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get('/bucket_info', async (req, res) => {
+  try {
+    res.send({
+      bucket: process.env.S3_BUCKET,
+      region: process.env.AWS_REGION
+    })
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
